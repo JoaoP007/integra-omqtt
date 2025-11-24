c17a2526c01a3f20657127c14a6b50cb428ce840
JoaoP007 <84014266+JoaoP007@users.noreply.github.com>
2025-11-24 16:33:37 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 const http = require("http");
 const RED = require("node-red");
 
+// Cria server HTTP
 const server = http.createServer();
 
+// Configura Node-RED
 const settings = {
-  uiPort: process.env.PORT || 1880,
   httpAdminRoot: "/",
-  httpNodeRoot: "/node",
+  httpNodeRoot: "/",
+  uiPort: process.env.PORT || 1880,
   functionGlobalContext: {}
 };
 
+// Inicializa Node-RED
 RED.init(server, settings);
 
-server.listen(settings.uiPort);
+// Roteia requisições HTTP para o Node-RED
+server.on("request", (req, res) => {
+  RED.httpAdmin(req, res);
+  RED.httpNode(req, res);
+});
 
+// Sobe o servidor
+server.listen(settings.uiPort, () => {
+  console.log("Node-RED rodando na porta", settings.uiPort);
+});
+
+// Inicia os fluxos
 RED.start();
